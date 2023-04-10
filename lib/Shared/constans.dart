b77e7a1a2dcbb912e57c827fed1f877e6cf5ebad
shadymohamed532001 <shadysteha571@gmail.com>
2023-04-10 12:20:28 +0200

    Chat App

---
+++ lib/Shared/constans.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+const KPrimaryColor = Color(0xff1D2223);
+const KLoginScreen = "LoginScrean";
+const KChatScrean = "ChatScrean";
+const KRegisterScrean = "RegisterScrean";
+const KMassageCollection = "massages";
+const Kmassage = "massage";
+const KDate = "Date";
+
+
+String? FirstName;
+String? LastName;
+String? Phone;
